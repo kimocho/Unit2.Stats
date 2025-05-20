6b150b8e17a8edf8e47313eeee252d0eb6e453d7
Samuel Kim <samjkim455@gmail.com>
2025-05-20 19:20:11 -0400

    final stat submission

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
 /**
@@ -11,7 +11,9 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  return numbers.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
 }
 
 /**
@@ -19,7 +21,9 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  // TODO
+  return numbers.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0) / numbers.length;
 }
 
 /**
@@ -27,7 +31,7 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
-  // TODO
+  return Math.min(...numbers);
 }
 
 /**
@@ -35,7 +39,7 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
-  // TODO
+  return Math.max(...numbers);
 }
 
 /**
@@ -43,7 +47,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return Math.max(...numbers) - Math.min(...numbers);
 }
 
 /**
@@ -51,7 +55,7 @@ function getRange(numbers) {
  * @returns {number[]} the even numbers in the array
  */
 function getEvens(numbers) {
-  // TODO
+  return numbers.filter(num => num % 2 === 0)
 }
 
 /**
@@ -59,7 +63,7 @@ function getEvens(numbers) {
  * @returns {number[]} the odd numbers in the array
  */
 function getOdds(numbers) {
-  // TODO
+  return numbers.filter(num => Math.abs(num) % 2 === 1)
 }
 
 // === READ BUT DO NOT EDIT THE CODE BELOW ===
